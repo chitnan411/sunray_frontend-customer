12af0eaef7ed4e8e41425d32bbfa77107d2040ca
chitnan <chintanthakkar.work@gmail.com>
2021-07-15 13:54:02 +0530

    change file url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 // export const apiRootUrl = "https://0571264e1618.ngrok.io"
 // export const apiRootUrl = "http://localhost:5000"
-export const apiRootUrl = "https://api.sunraystationers.com"
+export const apiRootUrl = "http://sunraystationers.com/api/"
 export const ssClientCryptoSecretKey = "yOqHPAqgdG"
 export const ssClientAuthFlagCookieKey = "a9K5J"
 export const ssClientAuthCookieKey = "_jUkq5291cN411"
